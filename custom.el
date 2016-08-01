8a819d51df00d346d44f6cac9e23b558a364f4d6
Sami Juvonen <sami@juvonen.org>
2016-08-01 16:54:18 -0700

    add custom.el

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (google-this pandoc-mode yaml-mode web-mode wcheck-mode volatile-highlights vagrant use-package solarized-theme rainbow-mode py-autopep8 puppet-mode projectile paredit ox-pandoc org-plus-contrib org-bullets markdown-mode magit hydra go-eldoc git-timemachine flycheck elpy ein diff-hl crux counsel company-quickhelp company-jedi company-emoji company-ansible beacon ansible-doc ansible aggressive-indent ack ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
