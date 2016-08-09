d1181d495454e64270c18c849f1b63ae705f8da3
Sami Juvonen <sami@juvonen.org>
2016-08-08 21:49:40 -0700

    Org babel/R seems to work again, enabled. Added benchmark-init.

--- custom.el
+++ custom.el
@@ -13,7 +13,8 @@
  '(org-gnome-integrate-with-calendar t)
  '(package-selected-packages
    (quote
-    (pdf-tools emacs-lorem-ipsum lorem-ipsum company-go org-gnome org-gcal inf-ruby docker dockerfile-mode expand-region google-this pandoc-mode yaml-mode web-mode wcheck-mode volatile-highlights vagrant use-package solarized-theme rainbow-mode py-autopep8 puppet-mode projectile paredit ox-pandoc org-plus-contrib org-bullets markdown-mode magit hydra go-eldoc git-timemachine flycheck elpy ein diff-hl crux counsel company-quickhelp company-emoji company-ansible beacon ansible-doc ansible aggressive-indent ack ace-window))))
+    (pdf-tools emacs-lorem-ipsum lorem-ipsum company-go org-gnome org-gcal inf-ruby docker dockerfile-mode expand-region google-this pandoc-mode yaml-mode web-mode wcheck-mode volatile-highlights vagrant use-package solarized-theme rainbow-mode py-autopep8 puppet-mode projectile paredit ox-pandoc org-plus-contrib org-bullets markdown-mode magit hydra go-eldoc git-timemachine flycheck elpy ein diff-hl crux counsel company-quickhelp company-emoji company-ansible beacon ansible-doc ansible aggressive-indent ack ace-window)))
+ '(safe-local-variable-values (quote ((ConTeXt-Mark-version . IV)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
